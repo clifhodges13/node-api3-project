40956920aa839d4262039f649af51f755c24d1ab
Clif <clifhodges13@gmail.com>
2019-12-10 21:35:04 -0600

    set up user endpoints

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// code away!
+const express = require('express');
+const server = require('./server')
+
+server.listen(5000, () => console.log('Server is running at http://localhost:5000'))
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,33 +1,48 @@
 const express = require('express');
+const db = require('./userDb');
 
 const router = express.Router();
 
 router.post('/', (req, res) => {
-  // do your magic!
+  db.insert(req.body)
+    .then(newUser => res.status(201).json(newUser))
+    .catch(err => res.status(500).json({ message: "An internal error occurred. Please try again later."}))
 });
 
-router.post('/:id/posts', (req, res) => {
-  // do your magic!
+router.post('/:id/posts', (req, res) => { // this one needs work
+  db.getUserPosts(req.body)
+    .then(post => res.status(201).json(post))
+    .catch(err => res.status(500).json({ message: "An internal error occurred. Please try again later."}))
 });
 
 router.get('/', (req, res) => {
-  // do your magic!
+  db.get()
+    .then(users => res.status(200).json(users))
+    .catch(err => res.status(500).json({ message: "An internal error occurred. Please try again later."}))
 });
 
 router.get('/:id', (req, res) => {
-  // do your magic!
+  db.getById(req.params.id)
+    .then(user => res.status(200).json(user))
+    .catch(err => res.status(500).json({ message: "An internal error occurred. Please try again later."}))
 });
 
 router.get('/:id/posts', (req, res) => {
-  // do your magic!
+  db.getUserPosts(req.params.id)
+    .then(posts => res.status(200).json(posts))
+    .catch(err => res.status(500).json({ message: "An internal error occurred. Please try again later."}))
 });
 
 router.delete('/:id', (req, res) => {
-  // do your magic!
+  db.remove(req.params.id)
+    .then(deleted => res.status(200).json({ deletedItems: deleted }))
+    .catch(err => res.status(500).json({ message: "An internal error occurred. Please try again later."}))
 });
 
 router.put('/:id', (req, res) => {
-  // do your magic!
+  db.update(req.params.id, req.body)
+    .then(updatedUser => res.status(200).json(updatedUser))
+    .catch(err => res.status(500).json({ message: "An internal error occurred. Please try again later."}))
 });
 
 //custom middleware
